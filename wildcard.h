8378e9434139976247ac9257a060dded95a3d35d
ianjray <ianjray@me.com>
2021-09-17 21:19:17 +0300

    Initial commit
    Signed-off-by: Ian Ray <ianjray@me.com>

---
+++ wildcard.h
@@ -0,0 +1,12 @@
+#ifndef WILDCARD__H
+#define WILDCARD__H
+
+
+/// @brief Match string to pattern
+/// @discussion Attempts to match string @c text with @c pattern.  The pattern
+/// may contain wildcards: ? matches any single character and * matches zero,
+/// one, or more characters.
+/// @returns int Zero on success or negative errno.
+int wildcard_match(const char *pattern, const char *text);
+
+#endif
